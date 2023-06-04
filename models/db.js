262b9c5b91eb644be1bf4a32358f9f3ad8605e0b
parthkamal <kamalparth40@gmail.com>
2023-06-05 02:00:38 +0530

    added admin cart navbar ejs

--- models/db.js
+++ models/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const createConnection = () => mongoose.connect(process.env.DB_CONNECTION_STRING,{useNewUrlParser: true})
-.catch((error) => console.log({error}));
+const createConnection = () => mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true })
+    .catch((error) => console.log({ error }));
 
 
-module.exports = {createConnection};
+module.exports = { createConnection };
 
 
 
---
+++ models/order.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose'); 
+
+
+const orderSchema = new mongoose.Schema({ 
+    order: {type: String},
+    total: {type: String}
+})
+
+
+module.exports = mongoose.model('Order', orderSchema);
