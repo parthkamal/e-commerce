44a365eff0e72b48feb8bf4a6b79d4cea0cd5eb8
parthkamal <kamalparth40@gmail.com>
2023-06-06 08:25:22 +0530

    added order routes , models, controller

--- controllers/order.js
+++ controllers/order.js
@@ -2,3 +2,104 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
+
+const getMenuController = (request,response) => {
+    response.render('menu');
+};
+
+
+const getCartController = (request,response) => {
+    response.render('cart');
+}
+
+const getOrderController = (request,response) => {
+    response.render('orders');
+}
+
+const getOrderByIdController = (request,response) => {
+    const {id } = request.params;
+
+
+    Order.findById(id, (error, result) => {
+        if(!error) {
+            response.render('orders', {order:result})
+        }else {
+            console.log({error});
+        }
+    })
+}
+
+const getDeleteOrderByIdController =  (request,response) => {
+    const {id} = request.params;
+    Order.findByIdAndRemove(id,(error,result) =>{
+        if(!error) {
+            response.redirect('/admin');
+        }else {
+            console.log({error});
+        }
+    })
+}
+
+
+
+const getAdminController = (request,response) => {
+    Order.find((error,docs) => {
+        if(!error){
+            response.render('admin',{order:docs});
+        }else {
+            console.log({error});
+        }
+    })
+}
+
+
+//post 
+
+
+const postCartController = (request,response) => {
+    const date = new Date();
+    const time = date.getTime();
+    let counter = time; 
+    counter +=1; 
+    const order = new Order();
+    order.total = request.body.total; 
+    order.order = counter; 
+    order.save((error,result) => {
+        if(!error){
+            console.log('orders;', order);
+            response.redirect('/admin');
+        }else {
+            console.log('error insert order 1 ',error);
+        }
+    })
+}
+
+
+const postOrderController = (request,response) => {
+    Order.findOneAndUpdate({_id: request.body._id}, request.body, {new:true}, (error, result) => {
+        if(!error) {
+            response.redirect('/admin');
+        }else {
+            console.log('update error' , {error});
+        }
+    })
+}
+
+
+
+
+module.exports = {
+    getMenuController,
+    getCartController,
+    getOrderByIdController,
+    getAdminController,
+    getOrderController,
+    getDeleteOrderByIdController,
+    postCartController,
+    postOrderController
+    
+}
+
+
+
+
--- routes/order.js
+++ routes/order.js
@@ -1,62 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const { getMenuController, getCartController, getOrderController, getAdminController, getOrderByIdController, getDeleteOrderByIdController, postCartController, postOrderController } = require('../controllers/order');
 const Order = mongoose.model('Order');
 
 
 mongoose.set('useFindAndModify',false);//configuration for mongodb to ignore deprecated methods. 
 
-router.get('/',(request,response) => {
-    response.render('menu');
-})
+router.get('/',getMenuController)
 
 
-router.get('/cart',(request,response) => {
-    response.render('cart');
-})
+router.get('/cart',getCartController)
 
 
-router.get('/orders',(request,response) => {
-    response.render('orders');
-})
+router.get('/orders',getOrderController)
 
 
-router.get('/admin',(request,response) => {
-    Order.find((error,docs) => {
-        if(!error){
-            response.render('admin',{order:docs});
-        }else {
-            console.log({error});
-        }
-    })
-})
+router.get('/admin',getAdminController)
 
 
-router.get('/order/:id',(request,response) => {
-    const {id } = request.params;
+router.get('/order/:id',getOrderByIdController)
 
 
-    Order.findById(id, (error, result) => {
-        if(!error) {
-            response.render('orders', {order:result})
-        }else {
-            console.log({error});
-        }
-    })
-})
-
-
-router.get('/order/delete/:id', (request,response) => {
-    const {id} = request.params;
-    Order.findByIdAndRemove(id,(error,result) =>{
-        if(!error) {
-            response.redirect('/admin');
-        }else {
-            console.log({error});
-        }
-    })
-});
+router.get('/order/delete/:id',getDeleteOrderByIdController);
 
 
 //post
+router.post('/cart' ,postCartController);
+
+router.post('/order',postOrderController);
+
+
 module.exports = router; 
